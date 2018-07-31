b9f5f1e93ee185fedc437f445f804792071dda97
danmcgill6 <danmcgill6@gmail.com>
2018-07-31 09:40:37 -0400

    fix: updates topten table using already existing db slots

--- db.js
+++ db.js
@@ -4,24 +4,17 @@ require('dotenv').config()
 
 const Sequelize = require('sequelize')
 
-//TODO(danmcgill6): MAKE THIS ONE LINE
-const app = require('./models/app')(sequelize, Sequelize)
-const blacklist = require('./models/blacklist')(sequelize, Sequelize)
-const developer = require('./models/developer')(sequelize, Sequelize)
-const topten = require('./models/top_ten')(sequelize, Sequelize)
-const article = require('./models/article')(sequelize, Sequelize)
-
-
 //USE STRING TEMPLATING HERE SET @BOMMING-APPS.... TO A VAR AND USE A TEMPLATE
-url_items = [
-    'postgres://',
-    process.env.PG_USERNAME,
-    ':',
-    process.env.PG_PASSWORD,
+const url_items = [
+    'postgres://' +
+    process.env.PG_USERNAME +
+    ':' +
+    process.env.PG_PASSWORD+
     '@bomming-apps.cdsxqnlho9ad.us-east-2.rds.amazonaws.com/booming_apps'
 ]
 
-db_URL = url_items.join()
+const db_URL = url_items.join()
+
 const sequelize = new Sequelize(db_URL, {
     define: {
         timestamps: false
@@ -34,6 +27,14 @@ const sequelize = new Sequelize(db_URL, {
         idle: 10000
     }
 })
+
+//TODO(danmcgill6): MAKE THIS ONE LINE
+const app = require('./models/app')(sequelize, Sequelize)
+const blacklist = require('./models/blacklist')(sequelize, Sequelize)
+const developer = require('./models/developer')(sequelize, Sequelize)
+const topten = require('./models/top_ten')(sequelize, Sequelize)
+const article = require('./models/article')(sequelize, Sequelize)
+
 
 const db = {
     Sequelize,
